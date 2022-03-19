d62c2e505476704d03d9122d0a0c5060e283b401
Delphine <delphine.pva@gmail.com>
2022-03-19 02:10:42 +0100

    Creation of a navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+
+import Image from 'next/image';
+
+import avatar from "../public/assets/avatar.png";
+import netflix from "../public/assets/netflix_logo.png";
+
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header className="header">
+            <Image src={netflix} alt="Netflix logo" className="header__logo" height={70} width={120} />
+            <nav className="header__primary__navigation">
+                <a href="/" className="header__item">Home</a>
+                <a href="/" className="header__item">Movies</a>
+                <a href="/" className="header__item">New & Popular</a>
+                <a href="/" className="header__item">My List</a>
+            </nav>
+            <div className="header__secondary__navigation">
+                <a href="/" className="header__link"><SearchIcon /></a>
+                <a href="/" className="header__link"><NotificationsIcon /></a>
+                <Image src={avatar} alt="Avatar" height={30} width={30} className="header__avatar" />
+            </div>
+
+        </header>
     );
 }
 
