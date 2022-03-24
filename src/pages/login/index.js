93ee4a557a14d2756643176eba218681c7c7ffa1
Delphine <delphine.pva@gmail.com>
2022-03-24 23:02:49 +0100

    New component Input created and added to login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import Image from 'next/image';
 
-import background from "../../public/assets/background.jpeg";
-
 import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import Background from "../../public/assets/background.jpeg";
 
 const Index = () => {
     return (
         <div>
-            <Image src={background} alt="Netflix background" className="" />
+            <Image src={Background} alt="Netflix background" className="" />
             <div className="overlay"></div>
-            <div className="modal">
-                <div className="modal__header" >
+            <div className="login">
 
-                    <div className='modal__content'>
-                        <h1 className='modal__title'></h1>
-                    </div>
-                </div>
-
-                <div className='modal__conten'>
-                    <p className='modal__description'></p>
-                    <p className='modal__description'>test</p>
-                    <p className='modal__description'>test</p>
-                    <Button title="Register" />
+                <div className='login__content'>
+                    <h1 className="login__title">Sign In</h1>
+                    <form>
+                        <Input
+                            // label="Email"
+                            name="email"
+                            id="email"
+                            type="text"
+                            // classes="login__input"
+                            required={false}
+                            placeholder="Email or phone number"
+                        />
+                        <Input
+                            // label="Email"
+                            name="password"
+                            id="password"
+                            type="text"
+                            // classes="login__input"
+                            required={false}
+                            placeholder="Password"
+                        />
+                    </form>
+                    <Button title="Sign In" />
                 </div>
             </div>
         </div>
